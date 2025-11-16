bd82cbe013eef267e75c46180c7b6895f5055362
ali0346 <haidet428@gmail.com>
2025-11-16 18:39:17 +0500

    dh fixed

--- app/crypto/dh.py
+++ app/crypto/dh.py
@@ -1,7 +1,3 @@
-# # """Classic DH helpers + Trunc16(SHA256(Ks)) derivation.""" 
-# # raise NotImplementedError("students: implement DH helpers")
-
-
 # """Classic DH helpers + Trunc16(SHA256(Ks)) derivation."""
 
 # import os
